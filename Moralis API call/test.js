ce1bd68693fb763158826b5b60786931ea24b0c5
thegamecat999 <164084979+thegamecat999@users.noreply.github.com>
2024-03-21 00:59:14 -0400

    Update test.js

--- Moralis API call/test.js
+++ Moralis API call/test.js
@@ -1,6 +1,6 @@
 const options = {
   method: 'GET',
-  headers: { 'Accept': 'application/json', 'X-API-Key': 'your API Key' },
+  headers: { 'Accept': 'application/json', 'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImFhMGZlYWViLWQyM2UtNGJjNC1iYjI1LWRjZTI2ZjA1YzI5YiIsIm9yZ0lkIjoiMzgzOTM5IiwidXNlcklkIjoiMzk0NTAyIiwidHlwZUlkIjoiYmIwM2QxMDQtNGY5Zi00MWUyLTg2N2MtODBhZDU5NTczYjcwIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MTA5OTYyNDYsImV4cCI6NDg2Njc1NjI0Nn0.CkjWNiyKYyJ-w7iFehUG9W3HotX0s9Cmwg6SMRlyCKg' },
 };
 fetch('https://deep-index.moralis.io/api/v2/block/1000?chain=eth', options)
   .then((response) => response.json())
